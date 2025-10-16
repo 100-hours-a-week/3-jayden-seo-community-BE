ebbd3b5dd8b4af08ed3d75fb170725fa19cf3c39
ert1015 <sde101573@gmail.com>
2025-10-16 15:49:38 +0900

    feat: 3000포트(FE)에 대한 CORS 허용

--- src/main/java/com/kakao_tech/community/Config/WebConfig.java
+++ src/main/java/com/kakao_tech/community/Config/WebConfig.java
@@ -4,6 +4,7 @@ import com.kakao_tech.community.Utils.LoginMemberArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +18,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
     }
 }
