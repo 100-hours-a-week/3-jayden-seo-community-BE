710dc89f3cddd0502dd82caa47a11224ce2a147e
ert1015 <sde101573@gmail.com>
2025-10-19 15:30:32 +0900

    refactor: 비밀번호 확인 필드명 수정

--- src/main/java/com/kakao_tech/community/Dto/Member/UpdatePasswordRequest.java
+++ src/main/java/com/kakao_tech/community/Dto/Member/UpdatePasswordRequest.java
@@ -14,5 +14,5 @@ public class UpdatePasswordRequest {
     private String password;
 
     @NotBlank(message = "비밀번호 확인은 필수입니다.")
-    private String confirmPassword;
+    private String passwordConfirm;
 }
