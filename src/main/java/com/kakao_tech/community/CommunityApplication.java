85327afc51b9f6d523b5439317e6a4c48742dac8
ert1015 <sde101573@gmail.com>
2025-11-05 13:09:56 +0900

    [feat] .env 추가

--- src/main/java/com/kakao_tech/community/CommunityApplication.java
+++ src/main/java/com/kakao_tech/community/CommunityApplication.java
@@ -1,5 +1,6 @@
 package com.kakao_tech.community;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class CommunityApplication {
 
 	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach((entry) -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
 		SpringApplication.run(CommunityApplication.class, args);
 	}
 
