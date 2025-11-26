caf10da75e1bbd9dad714b732f3aa00d1559ee45
ert1015 <sde101573@gmail.com>
2025-11-26 11:38:38 +0900

    [feat] test 환경 수정

--- src/test/java/com/kakao_tech/community/CommunityApplicationTests.java
+++ src/test/java/com/kakao_tech/community/CommunityApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class CommunityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
